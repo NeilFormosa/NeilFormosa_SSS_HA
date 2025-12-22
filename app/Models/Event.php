086473e38eda822148dad114afcd056955b5fe3e
Neil Formosa <neil.formosa.h63631@mcast.edu.mt>
2025-12-22 17:07:24 +0000

    Added models

---
+++ app/Models/Event.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+    
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
+
+    public function attendees()
+    {
+        return $this->hasMany(Attendee::class);
+    }
+}
